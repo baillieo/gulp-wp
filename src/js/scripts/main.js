f7453a7261cab87f98a97640bbe5a3376d0ca910
Baillie <baillieo@icloud.com>
2019-04-20 13:40:43 +0100

    organize js

--- src/js/scripts/main.js
+++ src/js/scripts/main.js
@@ -1,11 +1,16 @@
 (function() {
-  // header - mobile menu
+  // globals
   const header__burger = document.querySelector(".header__burger"),
     header__nav = document.querySelector(".header__nav"),
-    body = document.querySelector("body");
+    body = document.querySelector("body"),
+    WPI = document.querySelector("#posts");
+
+  // event listeners
   header__burger.addEventListener("click", function() {
     this.classList.toggle("active");
     header__nav.classList.toggle("open");
     body.classList.toggle("overflow-hidden");
   });
+
+  // API
 })();
