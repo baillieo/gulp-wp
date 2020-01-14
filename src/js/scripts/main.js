0d5b3fcb3ed80051c493ca19af5ebb020225c0c5
Baillie O'Grady <hello@baillieogrady.com>
2020-01-14 13:36:07 +0000

    Implement module javascript pattern

--- src/js/scripts/main.js
+++ src/js/scripts/main.js
@@ -1,16 +1,32 @@
-(function() {
-  // globals
-  const header__burger = document.querySelector(".header__burger"),
-    header__nav = document.querySelector(".header__nav"),
-    body = document.querySelector("body"),
-    WPI = document.querySelector("#posts");
+// Globals
+const global = {
+  body: document.querySelector("body")
+}
 
-  // event listeners
-  header__burger.addEventListener("click", function() {
-    this.classList.toggle("active");
-    header__nav.classList.toggle("open");
-    body.classList.toggle("overflow-hidden");
-  });
 
-  // API
+// Modules
+
+// header
+const header = (function(){
+  const content = {
+    headerBurger: document.querySelector(".header__burger"),
+    headerNav: document.querySelector(".header__nav")
+  }
+    
+  const burgerToggle = function() {
+    // toggle to mobile menu when burger icon is clicked
+    content.headerBurger.addEventListener("click", function() {
+      this.classList.toggle("active");s
+      content.headerNav.classList.toggle("open");
+      global.body.classList.toggle("overflow-hidden");
+    });
+  }
+
+  return {
+    callBurgerToggle: function() {
+      burgerToggle();
+    }
+  }
 })();
+
+header.callBurgerToggle();
